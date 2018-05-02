4d558f1e113b952f9b62c76e18d9326b6f58b539
rmacias6 <rmacias6@gatech.edu>
2018-05-02 11:18:12 -0500

    Added bulk insert Test

--- AndroidApp/app/src/androidTest/java/com/example/richie/leaveamessage/data/ContentProviderTest.java
+++ AndroidApp/app/src/androidTest/java/com/example/richie/leaveamessage/data/ContentProviderTest.java
@@ -38,7 +38,7 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
         super.tearDown();
     }
 
-
+    //Test Single Insert and Query
     public void testMessageInsert(){
         Uri uri = MessageContract.MessageEntry.CONTENT_URI.buildUpon().appendPath("1").build();
         uri = mMockResolver.insert(uri,
@@ -58,6 +58,29 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
         assertEquals(VALID_MESSAGE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
     }
 
+    //Test Bulk Insert and Query
+    public void testBulkInsert(){
+        Uri uri = MessageContract.MessageEntry.CONTENT_URI;
+        int rowsInserted = mMockResolver.bulkInsert(uri,
+                getMultipleMessagesCV());
+        Cursor cursor = mMockResolver.query(MessageContract.MessageEntry.CONTENT_URI,
+                null,
+                null,
+                new String[]{},
+                null);
+        assertNotNull(cursor);
+        assertEquals(2, cursor.getCount());
+        assertEquals(2, rowsInserted);
+        assertTrue(cursor.moveToFirst());
+        assertEquals(VALID_ID,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
+        assertEquals(VALID_LAT,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LAT)));
+        assertEquals(VALID_LON,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LON)));
+        assertEquals(VALID_DATE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_DATE)));
+        assertEquals(VALID_MESSAGE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+    }
+
+
+
     public static ContentValues getMessageContentValues(){
 
         ContentValues values = new ContentValues();
@@ -70,12 +93,34 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
         return values;
     }
 
+    public static ContentValues getMessageContentValues2(){
+
+        ContentValues values = new ContentValues();
+        values.put(MessageContract.MessageEntry.COLUMN_ID,VALID_ID_2);
+        values.put(MessageContract.MessageEntry.COLUMN_LAT,VALID_LAT);
+        values.put(MessageContract.MessageEntry.COLUMN_LON,VALID_LON);
+        values.put(MessageContract.MessageEntry.COLUMN_DATE,VALID_DATE);
+        values.put(MessageContract.MessageEntry.COLUMN_MESSAGE,VALID_MESSAGE_2);
+
+        return values;
+    }
+
+    public static ContentValues[] getMultipleMessagesCV(){
+        ContentValues cv1 = getMessageContentValues();
+        ContentValues cv2 = getMessageContentValues2();
+        ContentValues[] cv= {cv1,cv2};
+        return cv;
+    }
+
+    //Refactor to arrays of ids,lats, lon, etc and define a function to create an array of CVS in a loop.
     private static final int VALID_ID        = 1;
     private static final double VALID_LAT     = 41.9456354;
     private static final double VALID_LON    = -87.6679754;
     private static final String VALID_DATE    = "2018-05-01'T'12:12:32Z";
-
     private static final String VALID_MESSAGE = "Testing my Content Provider";
+
+    private static final int VALID_ID_2        = 2;
+    private static final String VALID_MESSAGE_2 = "Testing my Bulk Insert";
 
 
 }
