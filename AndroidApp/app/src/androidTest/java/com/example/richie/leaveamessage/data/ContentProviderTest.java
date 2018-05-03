d22764018a8340fa9f9fbfbacc73006fcd8e7274
rmacias6 <rmacias6@gatech.edu>
2018-05-03 10:02:04 -0500

    Tested Content Provider functions and passed

--- AndroidApp/app/src/androidTest/java/com/example/richie/leaveamessage/data/ContentProviderTest.java
+++ AndroidApp/app/src/androidTest/java/com/example/richie/leaveamessage/data/ContentProviderTest.java
@@ -72,11 +72,64 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
         assertEquals(2, cursor.getCount());
         assertEquals(2, rowsInserted);
         assertTrue(cursor.moveToFirst());
-        assertEquals(VALID_ID,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
+        assertEquals(VALID_ID_2,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
         assertEquals(VALID_LAT,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LAT)));
         assertEquals(VALID_LON,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LON)));
         assertEquals(VALID_DATE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_DATE)));
-        assertEquals(VALID_MESSAGE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+        assertEquals(VALID_MESSAGE_2,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+        cursor.moveToNext();
+        assertEquals(VALID_ID_3,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
+        assertEquals(VALID_LAT,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LAT)));
+        assertEquals(VALID_LON,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LON)));
+        assertEquals(VALID_DATE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_DATE)));
+        assertEquals(VALID_MESSAGE_3,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+    }
+
+    public void testDelete(){
+        Uri uri = MessageContract.MessageEntry.CONTENT_URI;
+        int rowsInserted = mMockResolver.bulkInsert(uri,
+                getMultipleMessagesCV());
+        int rowsdeleted = mMockResolver.delete(uri.buildUpon().appendPath("2").build(),null,null);
+        assertEquals(1,rowsdeleted);
+        Cursor cursor = mMockResolver.query(MessageContract.MessageEntry.CONTENT_URI,
+                null,
+                null,
+                new String[]{},
+                null);
+        assertNotNull(cursor);
+        assertEquals(1, cursor.getCount());
+        assertEquals(2, rowsInserted);
+        assertTrue(cursor.moveToFirst());
+        assertEquals(VALID_ID_3,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
+        assertEquals(VALID_LAT,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LAT)));
+        assertEquals(VALID_LON,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LON)));
+        assertEquals(VALID_DATE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_DATE)));
+        assertEquals(VALID_MESSAGE_3,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+    //I can test for delete all, but I prob won't need to ever use.
+    }
+
+    public void testUpdate(){
+
+        Uri uri = MessageContract.MessageEntry.CONTENT_URI.buildUpon().appendPath("1").build();
+        uri = mMockResolver.insert(uri,
+                getMessageContentValues());
+
+        int taskUpdated = mMockResolver.update(uri,getMessageContentValues2(),null,null);
+        Cursor cursor = mMockResolver.query(MessageContract.MessageEntry.CONTENT_URI,
+                null,
+                null,
+                new String[]{},
+                null);
+        assertEquals(1,taskUpdated);
+        assertNotNull(cursor);
+        assertEquals(1, cursor.getCount());
+        assertTrue(cursor.moveToFirst());
+        assertEquals(VALID_ID_2,cursor.getInt(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_ID)));
+        assertEquals(VALID_LAT,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LAT)));
+        assertEquals(VALID_LON,cursor.getDouble(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_LON)));
+        assertEquals(VALID_DATE,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_DATE)));
+        assertEquals(VALID_MESSAGE_2,cursor.getString(cursor.getColumnIndex(MessageContract.MessageEntry.COLUMN_MESSAGE)));
+
     }
 
 
@@ -105,10 +158,22 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
         return values;
     }
 
+    public static ContentValues getMessageContentValues3(){
+
+        ContentValues values = new ContentValues();
+        values.put(MessageContract.MessageEntry.COLUMN_ID,VALID_ID_3);
+        values.put(MessageContract.MessageEntry.COLUMN_LAT,VALID_LAT);
+        values.put(MessageContract.MessageEntry.COLUMN_LON,VALID_LON);
+        values.put(MessageContract.MessageEntry.COLUMN_DATE,VALID_DATE);
+        values.put(MessageContract.MessageEntry.COLUMN_MESSAGE,VALID_MESSAGE_3);
+
+        return values;
+    }
+
     public static ContentValues[] getMultipleMessagesCV(){
-        ContentValues cv1 = getMessageContentValues();
         ContentValues cv2 = getMessageContentValues2();
-        ContentValues[] cv= {cv1,cv2};
+        ContentValues cv3 = getMessageContentValues3();
+        ContentValues[] cv= {cv2,cv3};
         return cv;
     }
 
@@ -121,6 +186,9 @@ public class ContentProviderTest extends ProviderTestCase2<MessageProvider>{
 
     private static final int VALID_ID_2        = 2;
     private static final String VALID_MESSAGE_2 = "Testing my Bulk Insert";
+
+    private static final int VALID_ID_3        = 3;
+    private static final String VALID_MESSAGE_3 = "Testing my Bulk Insert twice";
 
 
 }
